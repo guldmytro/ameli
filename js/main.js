b609b88481c48ee0d1efafe667b15a6079893739
Dmytro Gul <guldmytro@gmail.com>
2022-04-10 19:42:49 +0300

    Added Catalog, Product page

--- js/main.js
+++ js/main.js
@@ -6,12 +6,25 @@ $(document).ready(function() {
         if ($this.hasClass('active')) {
             return false;
         }
-        $('.bestsellers-item__header.active').removeClass('active').next('.bestsellers-item__content').slideUp(200);
-        $this.addClass('active').next('.bestsellers-item__content').slideDown(200);;
+        if ($this.is('.bestsellers-item__header')) {
+            $('.bestsellers-item__header.active').removeClass('active').next('.bestsellers-item__content').slideUp(200);
+            $this.addClass('active').next('.bestsellers-item__content').slideDown(200);
+        } else {
+            $('.accordeon-item__header.active').removeClass('active').next('.accordeon-item__description').slideUp(200);
+            $this.addClass('active').next('.accordeon-item__description').slideDown(200);
+        }
     }
-    $('.bestsellers-item__content').hide();
-    const bestsellersAccordeon = $('.bestsellers-item__header');
-    bestsellersAccordeon.click(toggleAccordeon);
+    $('.bestsellers-item__header:not(.active) + .bestsellers-item__content, .accordeon-item__header:not(.active) + .accordeon-item__description').hide();
+    const accordeon = $('.bestsellers-item__header, .accordeon-item__header');
+    accordeon.click(toggleAccordeon);
+
+    // Sliders
+    $('.related-slider').flickity({
+        cellAlign: 'left',
+        contain: true,
+        pageDots: false,
+        wrapAround: true,
+    });
     
 });
 // end ready
